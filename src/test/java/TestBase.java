bce1b3d0309ed4fc1cbb20dd76ecad7615f85e0d
Sem <svgulbis92@gmail.com>
2022-02-14 22:50:46 +0700

    Fix TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,14 +1,26 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
-    static void beforeAll() {
-        Configuration.startMaximized = true;
-    }
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
 
     @AfterEach
     public void tearDown() {
@@ -16,5 +28,6 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        closeWebDriver();
     }
 }
